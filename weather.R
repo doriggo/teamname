a1086fd8474a5f8d4aed7a905e27bdd368395e00
HamYiHyeon <163076871+HamYiHyeon@users.noreply.github.com>
2024-04-16 14:15:41 +0900

    Add files via upload

--- weather.R
+++ weather.R
@@ -4,19 +4,29 @@ setwd('G:\\내 드라이브\\대학\\2-1\\확률과통계\\Rstudio 작업공간'
 weather <- read.csv('weather.csv', header=T)
 head(weather)
 
-date <- dlgInput('today date')$res
-point <- as.numeric(dlgInput('point')$res)
-name <- dlgInput('name')$res
-average <- as.numeric(dlgInput('averager')$res)
-min <- as.numeric(dlgInput('min')$res)
-max <- as.numeric(dlgInput('max')$res)
-
-weather.today <- data.frame(date, point, name,average, min, max)
-weather.today
+for (i in 1:2) {
+  date <- dlgInput('week date')$res
+  point <- as.numeric(dlgInput('point')$res)
+  name <- dlgInput('name')$res
+  average <- as.numeric(dlgInput('averager')$res)
+  min <- as.numeric(dlgInput('min')$res)
+  max <- as.numeric(dlgInput('max')$res)
+  weather <- rbind(weather, c(date, point, name,average, min, max))
+  
+  
+  if(min < 0) {
+    cat(date, "의 최저기온은", min, "으로 매우 춥습니다.\n")
+  }
+  else if(min == 0) {
+    cat(date, "의 최저기온은", min, "으로 조금 쌀쌀합니다.\n")
+  }
+  else {
+    cat(date, "의 최저기온은", min, "입니다.\n")
+  }
+}
 
 sink('Weather.txt', append=F)
 print(weather)
-print(weather.today, header=F)
 sink()
 
 weather.average.cold <- subset(weather, average <= 0)
